71d16cb98640448d7e0a541a69c5eb5ad4c60e20
AbiollaghJames <abiollajames@protonmail.com>
2024-01-06 10:48:19 +0300

    AbiollaghJames Portfolio website

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -1,11 +1,18 @@
 // Imports
 const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 
 // Environment variable
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || "127.0.0.1";
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Static Files
 app.use(express.static("public"));
@@ -22,6 +29,39 @@ app.set("view engine", "ejs");
 // Serving HTML pages
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+// Handling form submission
+app.post("/submit-form", (req, res) => {
+    const { name, email, message } = req.body;
+
+    // Creating nodemailer transporter
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "abiollajames@gmail.com",
+            pass: process.env.PASS
+        }
+    });
+
+    // Email options
+    const mailOptions = {
+        from: email,
+        to: "abiollajames@gmail.com",
+        subject: "Rely from Abiollagh James",
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    // Sending Email
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send("Internal Server Error!");
+        } else {
+            console.log("Email sent: " + info.resposne);
+            res.status(200).send("Email sent successfully");
+        }
+    });
 });
 
 // Listening port
